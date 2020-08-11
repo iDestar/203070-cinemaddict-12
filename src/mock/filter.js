a2f52e5595904e445b53a3985764a6957997f419
iDestar <destarpwnz@gmai.com>
2020-08-11 20:24:25 +0300

    Update filter.js

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -1,14 +1,9 @@
 export const generateNavigationFilter = (cards) => {
 
   return {
-    watchlist: cards.filter(function (card) {
-      return card.isWatchlist;
-    }).length,
-    watched: cards.filter(function (card) {
-      return card.isWatched;
-    }).length,
-    favorites: cards.filter(function (card) {
-      return card.isFavorit;
-    }).length,
+    all: cards.length,
+    watchlist: cards.filter((card) => card.isWatchlist).length,
+    watched: cards.filter((card) => card.isWatched).length,
+    favorites: cards.filter((card) => card.isFavorit).length,
   };
 };
