59333a78c96cf6691ed2caf4206c96b7c2c6bc11
iDestar <destarpwnz@gmai.com>
2020-08-31 01:44:28 +0300

    7 - 1

--- src/const.js
+++ src/const.js
@@ -84,3 +84,21 @@ export const SortType = {
   DATE: `date`,
   RATING: `rating`,
 };
+
+export const UserAction = {
+  UPDATE_FILM: `UPDATE_FILM`,
+  DELETE_COMMENT: `DELETE_COMMENT`
+};
+
+export const UpdateType = {
+  PATCH: `PATCH`,
+  MINOR: `MINOR`,
+  MAJOR: `MAJOR`
+};
+
+export const FilterType = {
+  ALL: `all`,
+  WATCHLIST: `watchlist`,
+  HISTORY: `history`,
+  FAVORITES: `favorites`
+};
---
+++ src/observer.js
@@ -0,0 +1,17 @@
+export default class Observer {
+  constructor() {
+    this._observers = [];
+  }
+
+  addObserver(observer) {
+    this._observers.push(observer);
+  }
+
+  removeObserver(observer) {
+    this._observers = this._observers.filter((existedObserver) => existedObserver !== observer);
+  }
+
+  _broadcast(event, payload) {
+    this._observers.forEach((observer) => observer(event, payload));
+  }
+}
