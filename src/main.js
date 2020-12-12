5113cf51211a6355e908fd61b391d08786caa383
iDestar <destarpwnz@gmai.com>
2020-12-12 07:16:40 +0300

    s

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 "use strict";
 
 const render = (container, template, place) => {
+<<<<<<< Updated upstream
     container.insertAdjacentHTML(place, template);
+=======
+  container.insertAdjacentHTML(place, template);
+>>>>>>> Stashed changes
 };
