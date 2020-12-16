bf48584bb55e2cedf3f129f53cd3ca2220bb54bc
iDestar <destarpwnz@gmai.com>
2020-12-16 03:22:56 +0300

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-const CARD_COUNT = 5;
+import { profileComponent } from "./view/profile-component";
+import { mainMenuComponent } from "./view/main-menu-component";
+import { sortComponent } from "./view/sort-component";
+import { filmListSectionComponent } from "./view/film-section-component";
 
+const CARD_COUNT = 5;
 const header = document.querySelector(".header");
 const main = document.querySelector(".main");
 
@@ -7,74 +11,9 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const profileComponent = () => {
-  return `<section class="header__profile profile">
-    <p class="profile__rating">Movie Buff</p>
-    <img
-      class="profile__avatar"
-      src="images/bitmap@2x.png"
-      alt="Avatar"
-      width="35"
-      height="35"
-    />
-  </section>`;
-};
-
 render(header, profileComponent(), `beforeend`);
-
-const mainMenuComponent = () => {
-  return `<nav class="main-navigation">
-    <div class="main-navigation__items">
-      <a
-        href="#all"
-        class="main-navigation__item main-navigation__item--active"
-      >
-        All movies
-      </a>
-      <a href="#watchlist" class="main-navigation__item">
-        Watchlist <span class="main-navigation__item-count">13</span>
-      </a>
-      <a href="#history" class="main-navigation__item">
-        History <span class="main-navigation__item-count">4</span>
-      </a>
-      <a href="#favorites" class="main-navigation__item">
-        Favorites <span class="main-navigation__item-count">8</span>
-      </a>
-    </div>
-    <a href="#stats" class="main-navigation__additional">
-      Stats
-    </a>
-  </nav>`;
-};
-
 render(main, mainMenuComponent(), `beforeend`);
-
-const sortComponent = () => {
-  return `<ul class="sort">
-    <li>
-      <a href="#" class="sort__button sort__button--active">
-        Sort by default
-      </a>
-    </li>
-    <li>
-      <a href="#" class="sort__button">
-        Sort by date
-      </a>
-    </li>
-    <li>
-      <a href="#" class="sort__button">
-        Sort by rating
-      </a>
-    </li>
-  </ul>`;
-};
-
 render(main, sortComponent(), `beforeend`);
-
-const filmSectionComponent = () => {
-  return `<section class="films"></section>`;
-};
-
 render(main, filmSectionComponent(), `beforeend`);
 
 const filmSection = document.querySelector(".films");
---
+++ src/view/film-section-component.js
@@ -0,0 +1,5 @@
+const filmSectionComponent = () => {
+  return `<section class="films"></section>`;
+};
+
+export { filmSectionComponent };
